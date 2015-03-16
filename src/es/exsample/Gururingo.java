a8b2ff9a9091ec8852fce3b3b6a0353afb09f2f8
kobakei <kkeisukee222@gmail.com>
2015-03-16 22:32:42 +0900

    GIT練習としてのコミット

--- src/es/exsample/Gururingo.java
+++ src/es/exsample/Gururingo.java
@@ -27,68 +27,70 @@ public class Gururingo extends Activity{
 	private DataBaseManager mDB;
 	private List<HashMap<String,String>> dbList;
 	private int selectPosition=0;
-	
+
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.search_bus);
-		
+
 		mDB = new DataBaseManager(this);
 		db =  mDB.getWritableDatabase();
-		
+
 		//mDB.showData(db);
-		
+
 		dbList = getItemList();
 		adapter = new SimpleAdapter(getApplicationContext(),
                 dbList, R.layout.list_view_item,
                 new String[] { "title" }, new int[] { R.id.text_view1 });
-         
+
         listView = (ListView) findViewById(R.id.listView1);
         listView.setAdapter(adapter);
         listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
         listView.setItemChecked(0, true);
         listView.setOnItemClickListener(listenClickItem);
-        
+
         Button bt = (Button)findViewById(R.id.button1);
-        bt.setOnClickListener(listenClick);        
-        
+        bt.setOnClickListener(listenClick);
+
 		db.close();
-		
+
 	}
-	
+
+	//gitの練習で入れただけの１行
+
 	private ArrayList<HashMap<String, String>> getItemList() {
         ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
         Cursor c = db.rawQuery("select name from " + mDB.TABLE_NAME, null);
-        
+
         if(c.moveToFirst()){
     		do{
     	        HashMap<String, String> item = new HashMap<String, String>();
     	        String str = c.getString(0);
-    	        
+
     	        item.put("title", str);
     			list.add(item);
     		}while(c.moveToNext());}
-        
+
         return list;
     }
-	
+
 	public OnClickListener listenClick = new OnClickListener(){
 		public void onClick(View v) {
 			String str = dbList.get(selectPosition).get("title");
 			Intent intent = new Intent();
 			intent.setClassName("es.exsample","es.exsample.Result");
 	        intent.putExtra("selectBus", str);
-	 
+
 			startActivity(intent);
 		}
 
 	};
-	
+
 	public OnItemClickListener listenClickItem = new OnItemClickListener(){
 		public void onItemClick(AdapterView<?> parent, View view, int position,
 				long id) {
-			
+
 			selectPosition = position;
 		}
-		
+
 	};
 }
